ab552267b2cd310ab9cbb27d13a48b28070ad8dc
MRaehanR <raehanrobban2@gmail.com>
2023-02-24 08:54:14 +0700

    feat: register form doctor

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -8,79 +8,168 @@ use Illuminate\Support\Facades\Log;
 
 class FormController extends Controller
 {
-    public function registerCustomer()
+    public function register()
     {
-        try {
-            return response()->json([
-                [
-                    'label' => 'Name',
-                    'name' => 'name',
-                    'type' => 'text',
-                    'required' => true,
-                    'prepend_inner_icon' => 'mdi-account-circle',
-                ],
-                [
-                    'label' => 'Email',
-                    'name' => 'email',
-                    'type' => 'email',
-                    'required' => true,
-                    'prepend_inner_icon' => 'mdi-email',
-                ],
-                [
-                    'label' => 'Password',
-                    'name' => 'password',
-                    'type' => 'password',
-                    'required' => true,
-                ],
-                [
-                    'label' => 'Confirmation Password',
-                    'name' => 'password_confirmation',
-                    'type' => 'password',
-                    'required' => true,
-                ],
-                [
-                    'label' => 'Phone',
-                    'name' => 'phone',
-                    'type' => 'phone',
-                    'required' => true,
-                    'prepend_inner_icon' => 'mdi-phone',
-                ],
-                [
-                    'label' => 'Photo Profile',
-                    'name' => 'photo',
-                    'type' => 'file',
-                    'required' => false,
-                ],
-                [
-                    'label' => 'Gender',
-                    'name' => 'gender',
-                    'type' => 'select',
-                    'required' => true,
-                    'options' => [
-                        [
-                            'label' => 'Male',
-                            'value' => 'm',
-                        ],
-                        [
-                            'label' => 'Female',
-                            'value' => 'f',
-                        ],
-                    ]
-                ],
-                [
-                    'label' => 'Role',
-                    'name' => 'role',
-                    'type' => 'hidden',
-                    'required' => true,
-                    'value' => 3
-                ],
-            ]);
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        return response()->json([
+            [
+                'label' => 'Name',
+                'name' => 'name',
+                'type' => 'text',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-account-circle',
+            ],
+            [
+                'label' => 'Email',
+                'name' => 'email',
+                'type' => 'email',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-email',
+            ],
+            [
+                'label' => 'Password',
+                'name' => 'password',
+                'type' => 'password',
+                'required' => true,
+            ],
+            [
+                'label' => 'Confirmation Password',
+                'name' => 'password_confirmation',
+                'type' => 'password',
+                'required' => true,
+            ],
+            [
+                'label' => 'Provinsi',
+                'name' => 'province_id',
+                'type' => 'select',
+                'required' => true,
+                'options' => [],
+            ],
+            [
+                'label' => 'Kota',
+                'name' => 'city_id',
+                'type' => 'select',
+                'required' => true,
+                'options' => [],
+            ],
+            [
+                'label' => 'Phone',
+                'name' => 'phone',
+                'type' => 'phone',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-phone',
+            ],
+            [
+                'label' => 'Photo Profile',
+                'name' => 'photo',
+                'type' => 'file',
+                'required' => false,
+            ],
+            [
+                'label' => 'Gender',
+                'name' => 'gender',
+                'type' => 'select',
+                'required' => true,
+                'options' => [
+                    [
+                        'label' => 'Male',
+                        'value' => 'm',
+                    ],
+                    [
+                        'label' => 'Female',
+                        'value' => 'f',
+                    ],
+                ]
+            ],
+            [
+                'label' => 'Role',
+                'name' => 'role',
+                'type' => 'hidden',
+                'required' => true,
+                'value' => 3
+            ],
+        ]);
+    }
+
+    public function registerDoctor()
+    {
+        return response()->json([
+            [
+                'label' => 'Curriculum Vitae',
+                'name' => 'cv',
+                'type' => 'file',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-paperclip',
+            ],
+            [
+                'label' => 'Surat Tanda Registrasi',
+                'name' => 'str',
+                'type' => 'file',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-paperclip',
+            ],
+            [
+                'label' => 'Kartu Tanda Penduduk',
+                'name' => 'str',
+                'type' => 'file',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-paperclip',
+            ],
+            [
+                'label' => 'Jenis Dokter',
+                'name' => 'type_doctor_id',
+                'type' => 'select',
+                'required' => true,
+                'options' => [
+                    [
+                        'label' => 'Sp. Mata',
+                        'value' => 1,
+                    ],
+                    [
+                        'label' => 'Sp. THT',
+                        'value' => 2,
+                    ],
+                    [
+                        'label' => 'Sp. kulit & kelamin',
+                        'value' => 3,
+                    ],
+                    [
+                        'label' => 'Dokter Gigi',
+                        'value' => 4,
+                    ],
+                    [
+                        'label' => 'Sp. Konservasi Gigi',
+                        'value' => 5,
+                    ],
+                ]
+            ],
+            [
+                'label' => 'Pengalaman',
+                'name' => 'experience',
+                'type' => 'number',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-timer-outline',
+            ],
+            [
+                'label' => 'Alumnus',
+                'name' => 'alumnus',
+                'type' => 'string',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-school',
+            ],
+            [
+                'label' => 'Tahun Lulus',
+                'name' => 'alumnus_tahun',
+                'type' => 'number',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-timer-outline',
+            ],
+            [
+                'label' => 'Tempat Praktik',
+                'name' => 'tempat_praktik',
+                'type' => 'string',
+                'required' => true,
+                'prepend_inner_icon' => 'mdi-hospital-building',
+            ],
+        ]);
     }
 }
