e9ac8496fcd028b0001125d4db66543d06600717
MRaehanR <raehanrobban2@gmail.com>
2023-06-20 08:02:00 +0700

    refactor: email verification

--- app/Http/Controllers/Auth/EmailVerificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -13,53 +13,22 @@ class EmailVerificationController extends Controller
 {
     public function sendVerificationEmail(Request $request)
     {
-        try {
-            if ($request->user()->hasVerifiedEmail()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Already Verified',
-                ], Response::HTTP_ALREADY_REPORTED);
-            }
-    
-            $request->user()->sendEmailVerificationNotification();
-    
-            return response()->json([
-                'status' => true,
-                'message' => 'Verification link sent',
-            ], Response::HTTP_OK);
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        if ($request->user()->hasVerifiedEmail())
+            return response()->error('Already Verified', Response::HTTP_ALREADY_REPORTED);
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return response()->success('Verification link sent', Response::HTTP_OK);
     }
 
     public function verify(EmailVerificationRequest $request)
     {
-        try {
-            if ($request->user()->hasVerifiedEmail()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Email already verified',
-                ], Response::HTTP_ALREADY_REPORTED);
-            }
-    
-            if ($request->user()->markEmailAsVerified()) {
-                event(new Verified($request->user()));
-            }
-    
-            return response()->json([
-                'status' => true,
-                'message' => 'Email has been verified',
-            ], Response::HTTP_OK);
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        if ($request->user()->hasVerifiedEmail())
+            return response()->error('Already Verified', Response::HTTP_ALREADY_REPORTED);
+
+        if ($request->user()->markEmailAsVerified())
+            event(new Verified($request->user()));
+
+        return response()->success('Email has been verified', Response::HTTP_OK);
     }
 }
